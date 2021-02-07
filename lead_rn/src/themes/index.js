c1489c0f6c6163648fd4b544bedeb31fa195bf38
diwei77chen <diwei77chen@gmail.com>
2021-02-07 13:34:42 +1100

    added app themes

---
+++ lead_rn/src/themes/index.js
@@ -0,0 +1,26 @@
+const themes = {
+  primaryColor: '#ffa500',
+  secondaryColor: '#808080',
+  secondaryText: {
+    color: 'grey'
+  },
+  gapRight: {
+    marginRight: 10
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+    marginRight: 10,
+  },
+  card: {
+    marginTop: 25,
+    marginLeft: 10,
+    marginRight: 10
+  },
+  boldText: {
+    fontWeight: 'bold'
+  }
+}
+
+export default themes;
