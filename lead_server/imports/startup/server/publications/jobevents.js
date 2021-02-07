aae3a545e337653fd380b169f1010c9f9da65443
diwei77chen <diwei77chen@gmail.com>
2021-02-07 13:18:46 +1100

    added my_job_events publication

--- lead_server/imports/startup/server/publications/jobevents.js
+++ lead_server/imports/startup/server/publications/jobevents.js
@@ -1,15 +1,13 @@
-import { Meteor } from 'meteor/meteor';
-import { Publications } from '../utils/consts/publications';
-import { JobEvents } from '../collections/jobevents';
-// User is used to mock a logged-in user
-import User from '../dataset/user';
+// import { Meteor } from 'meteor/meteor';
+// import { Publications } from '../utils/consts/publications';
+// import { JobEvents } from '../collections/jobEvents';
+// // User is used to mock a logged-in user
+// import User from '../dataset/user';
 
-Meteor.publish(Publications.MY_JOB_EVENTS, function() {
-    if (!User) return new Error("Please login to perform the operation.");
-    // TODO only return the jobs that the user is interested in, filtered by categories or suburbs
-    const categoriesIDs = JobEvents.find().map(({ data }) => {
-        const { category_id } = data || {};
-        return category_id;
-    })
-    return Categories.find({ _id: { $in: categoriesIDs }  });
-});
+// Meteor.publish(Publications.MY_JOB_EVENTS, function() {
+//     if (!User) return new Error("Please login to perform the operation.");
+//     // TODO only return the jobs that the user is interested in, filtered by categories or suburbs
+//     // TODO pagination - use a variable to limit only returning the first [limit] of job events
+//     console.log(`Subscribing ${Publications.MY_JOB_EVENTS}`);
+//     return JobEvents.find({});
+// });
