6dc6853160930d64fe043dd213dec41cda44e230
diwei77chen <diwei77chen@gmail.com>
2021-02-07 21:20:17 +1100

    added comments for buildJobEvent

--- lead_server/imports/startup/server/utils/helpers/jobEvents.js
+++ lead_server/imports/startup/server/utils/helpers/jobEvents.js
@@ -1,3 +1,10 @@
+/**
+ * format a job event
+ * @param  {string} jid The job ID
+ * @param  {string} type The job event type
+ * @param  {object} data The job data
+ * @param  {string} author The user name who creates the job event
+ */
 export const buildJobEvent = (jid, type, data, author) => {
     return {
         jid,
