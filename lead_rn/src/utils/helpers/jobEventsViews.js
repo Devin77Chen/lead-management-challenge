9e6e459d5f7ed7a3237cfd5940f6f0aefbca5fd4
diwei77chen <diwei77chen@gmail.com>
2021-02-07 20:18:49 +1100

    added comments for buildLeads

--- lead_rn/src/utils/helpers/jobEventsViews.js
+++ lead_rn/src/utils/helpers/jobEventsViews.js
@@ -1,7 +1,13 @@
-export const buildLeads = (suburbs, categories, invitedJobEvents) => {
-    if (!Array.isArray(suburbs) || !Array.isArray(categories) || !Array.isArray(invitedJobEvents)) return [];
-    return invitedJobEvents.reduce((leads, jobEvent) => {
-        const { jid, timestamp, jobDetails } = jobEvent;
+/**
+ * build a lead array from a jobEvent list to be rendered on screen
+ * @param  {array} suburbs Suburbs collection data
+ * @param  {array} categories Categories collection data
+ * @param  {array} jobEvents JobEventsInvited or JobEventsAccepted collection data 
+ */
+export const buildLeads = (suburbs, categories, jobEvents) => {
+    if (!Array.isArray(suburbs) || !Array.isArray(categories) || !Array.isArray(jobEvents)) return [];
+    return jobEvents.reduce((leads, jobEvent) => {
+        const { jid, timestamp, jobDetails } = jobEvent || {};
         const { 
             status, 
             suburb_id, 
@@ -15,7 +21,7 @@ export const buildLeads = (suburbs, categories, invitedJobEvents) => {
         const suburb = suburbs.find(({ _id }) => _id === suburb_id);
         const category = categories.find(({ _id }) => _id === category_id);
         // Assumption: contact_name contains a person's first name and second name, they are separated by a space
-        const contact_name_first = contact_name.split(' ')[0];
+        const contact_name_first = typeof contact_name === 'string' && contact_name.split(' ')[0];
         const lead = {
             contact_name,
             contact_name_first,
