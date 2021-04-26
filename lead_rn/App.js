575a671736addee74cc6f28cde315b6de9c0b040
Devin77Chen <devin.chen@ofx.com>
2021-04-26 21:37:05 +1000

    config redux store

--- lead_rn/App.js
+++ lead_rn/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import 'react-native-gesture-handler';
 import Meteor from 'react-native-meteor';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -6,6 +7,7 @@ import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import Home from '@scenes/Home';
 import Settings from './settings';
 import themes from '@themes';
+import store from './src/store';
 
 Meteor.connect(Settings.SERVER_URL);
 
@@ -19,11 +21,13 @@ const navigationTheme = {
 
 const App: () => React$Node = () => {
   return (
-      <SafeAreaProvider>
-        <NavigationContainer theme={navigationTheme}>
-          <Home />
-        </NavigationContainer>
-      </SafeAreaProvider>
+      <Provider store={store}>
+        <SafeAreaProvider>
+          <NavigationContainer theme={navigationTheme}>
+            <Home />
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </Provider>
     );
 };
 
