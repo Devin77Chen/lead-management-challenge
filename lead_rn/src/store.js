031cd3ac7d53df6613365d03213286e5ba81a155
Devin77Chen <devin.chen@ofx.com>
2021-04-26 21:36:55 +1000

    update redux logger

--- lead_rn/src/store.js
+++ lead_rn/src/store.js
@@ -1,13 +1,11 @@
 import { applyMiddleware, compose, createStore } from 'redux';
-import createLogger from 'redux-logger';
+import logger from 'redux-logger';
 import { offline } from '@redux-offline/redux-offline';
 import offlineConfig from '@redux-offline/redux-offline/lib/defaults';
 import reducers from '@reducers';
 
-const logger = createLogger();
-const middleware = __DEV__ ? [logger] : [];
 const composeEnhancers = compose(
-    applyMiddleware(...middleware),
+    applyMiddleware(logger),
     offline(offlineConfig)
 )
 
